413aaa592eae5676baff490f820c39a64b9efc2f
MSI\hieud <hieudh@tgl-sol.com>
2025-12-02 02:13:26 +0700

    insert books

--- ebook-system-fe/src/features/layout/components/CRUDButtonList.tsx
+++ ebook-system-fe/src/features/layout/components/CRUDButtonList.tsx
@@ -1,5 +1,5 @@
 import { PlusOutlined } from "@ant-design/icons";
-import { useAppDispatch } from "../../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
 import { PATH, hasHeaderBtn } from "../../../constants/common";
 import { ActionItem, ActionWrapper } from "../LayoutHeader.style";
 import {
@@ -7,9 +7,11 @@ import {
   setIsUpdateForm,
   setSelectedRows,
 } from "../layoutSlice";
+import { selectPermissions } from "../../Login/LoginSlice";
 
 export default function CRUDButtonList({ path }: { path: string }) {
   const dispatch = useAppDispatch();
+  const activePermissions = useAppSelector(selectPermissions);
 
   const drawerOnOpenCreate = () => {
     dispatch(setIsUpdateForm(false));
@@ -17,9 +19,18 @@ export default function CRUDButtonList({ path }: { path: string }) {
     dispatch(setSelectedRows([]));
   };
 
+  const moduleName = path.split("/").filter(Boolean).pop();
+  console.log(activePermissions, moduleName);
+
+  const canCreate = activePermissions.some((slug: string) => {
+    const hasCreateAction = slug.includes("create");
+    const matchesModule = moduleName ? slug.includes(moduleName) : false;
+    return hasCreateAction && matchesModule;
+  });
+
   return (
     <>
-      {`${hasHeaderBtn}`.includes(path) || path.includes("/csvc/") ? (
+      {canCreate ? (
         <ActionWrapper>
           <ActionItem onClick={drawerOnOpenCreate}>
             <PlusOutlined style={{ color: "green" }} />
